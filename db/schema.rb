5cb41584883bd231d133e39ea4a0ac599ba6482b
Spicy Oranges Nitro <spicyoranges@icloud.com>
2014-11-16 23:19:11 +0000

    unmerged schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 20141107010348) do
-
-  create_table "users", force: true do |t|
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-end
-=======
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -54,4 +29,3 @@ ActiveRecord::Schema.define(version: 20141116213210) do
   end
 
 end
->>>>>>> 935afd0ee0a438806bb09051f3bdcabc3c542a43
