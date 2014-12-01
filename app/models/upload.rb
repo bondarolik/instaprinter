8db6734c8be51ab12055c3ce3a3ce8aa03127fa6
Spicy Oranges <spicyoranges@icloud.com>
2014-12-01 14:29:27 -0300

    Added validation for uploader

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,3 +1,4 @@
 class Upload < ActiveRecord::Base
   has_attached_file :uploaded_file, styles: { original: "1500x1500>", large: "1000x1000>", medium: "500x500>", small: "200x200>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
