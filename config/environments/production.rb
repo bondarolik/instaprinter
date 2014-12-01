9f2e134f10d55f4b0a546ecc8ce5d005c87f5179
Spicy Oranges <spicyoranges@icloud.com>
2014-12-01 13:58:28 -0300

    Production log setted to on

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,8 @@ SpicyOrangesWebsite::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  # The available log levels are: :debug, :info, :warn, :error, :fatal.
+  config.log_level = :error
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
