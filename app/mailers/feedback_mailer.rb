3ff6cf4c5bae68fdf4218916702a4595e6caf689
Spicy Oranges <spicyoranges@icloud.com>
2014-12-27 17:35:10 -0300

    Delivery mail options for smtp

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,11 +1,15 @@
 class FeedbackMailer < ActionMailer::Base
-  default to: "gobets96@gmail.com"
+  #default to: "gobets96@gmail.com"
+  default to: "ventas@alumart.cl"
 
   def feedback_email(name, email, body)
     @name = name
     @email = email
     @body = body
+    delivery_options = { user_name: "ventas@alumart.cl",
+                         password: "starsfgd",
+                         address: "smtp.gmail.com" }
 
-    mail(from: email, subject: "Feedback Alumart.cl de #{name}")
+    mail(from: email, subject: "Feedback Alumart.cl de #{name}", delivery_method_options: delivery_options)
   end
 end
